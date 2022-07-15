2744160bf7bf43bc5470391725b2289ef46fa75a
mlindsey23 <107881040+mlindsey23@users.noreply.github.com>
2022-07-15 13:30:31 -0400

    Create main.py

---
+++ BioeffectModeling/main.py
@@ -0,0 +1,29 @@
+from BioeffectModeling import *
+
+
+### USER PARAMETERS ###
+
+# 1. Path to DICOM files
+basepath = '/Users/mjlindsey/Documents/Liver Patients/Patient12/'
+
+# 2. Path to RTDOSE file
+dosepath = '/Users/mjlindsey/Documents/Liver Patients/Patient12/DoseOnCTGrid.dcm'
+
+# 3. Path to RTSTRUCT file
+structpath = '/Users/mjlindsey/Documents/Liver Patients/Patient12/RTSTRUCT/2.16.840.1.114362.1.11972228.22981817573.591918584.369.1694.dcm'
+
+# 4. Highest Voxel Value, to scale the output (NEEDS UPDATE)
+HighestVoxelValue = 60
+
+# 5. (optional) Alternate path to CT files (Default: ~/basepath/CT/ )
+ctpath = ''
+
+# 6. (optional) Alternate path to NM files (Default: ~/basepath/NM/ )
+nmpath = ''
+
+
+
+### MAIN ###
+
+GetBEDinDICOM(basepath, dosepath, structpath, ctpath, nmpath, HighestVoxelValue)
+
