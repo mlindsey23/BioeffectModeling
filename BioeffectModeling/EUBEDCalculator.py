946c6862dd1cea4d8a9b8491e534c8a56cea6dbf
mlindsey23 <107881040+mlindsey23@users.noreply.github.com>
2022-07-24 22:39:59 -0400

    Update and rename BEDCalculator.py to EUBEDCalculator.py

--- BioeffectModeling/EUBEDCalculator.py
+++ BioeffectModeling/EUBEDCalculator.py
@@ -41,8 +41,12 @@ class BioeffectCalculator(dcmpat.PatientCT):
             for j in range(self.ctObject.quantitiesOfInterest[0].array.shape[1]): 
                 for k in range(self.ctObject.quantitiesOfInterest[0].array.shape[2]):
                     if self.ctObject.structures3D['Liver'][i,j,k] == True :
-                        Trep = Trep_Normal
-                        AlphaBeta = AlphaBeta_NLiver
+                        if self.ctObject.structures3D['Right Tumor(s)'][i,j,k] == True or self.ctObject.structures3D['Left Tumor(s)'][i,j,k] == True or self.ctObject.structures3D['All Tumors (Left Lobe)'][i,j,k] == True or self.ctObject.structures3D['All Tumors (Right Lobe)'][i,j,k] == True:
+                            Trep = Trep_Tumor
+                            AlphaBeta = AlphaBeta_TLiver
+                        else:
+                            Trep = Trep_Normal
+                            AlphaBeta = AlphaBeta_NLiver
                     elif self.ctObject.structures3D['Lung_L'][i,j,k] == True or self.ctObject.structures3D['Lung_R'][i,j,k] == True :
                         Trep = Trep_Normal
                         AlphaBeta = AlphaBeta_NLung
@@ -57,23 +61,42 @@ class BioeffectCalculator(dcmpat.PatientCT):
         except:
             unit = 'arb. units'
         name = 'BEDCalculation_' + self.dosefilename + '.dcm'
-        
-        mCi = 37 #1mCi = 37 MBq
-        Gy = 1e3 #1 Gy = 1000 mGy
-        fn = 1
-        if unit == 'mGy/mCi':
-            fn = 1/mCi
-        elif unit == 'Gy/MBq':
-            fn = Gy
-        elif unit == 'Gy/mCi':
-            fn = Gy/mCi
-        else :
-            pass
-        
-        self.BEDimg3D = self.BEDimg3D / fn
         self.ctObject.WriteRTDose(self.BEDimg3D, self.basepath + name, unit)
 
-        
+    def EUBEDCalculator(self, ROIList):
+        for r in ROIList:
+            # Establish array with only ROI BED values
+            darr = np.zeros(self.ctObject.quantitiesOfInterest[0].array.shape) 
+            for i in range(self.ctObject.quantitiesOfInterest[0].array.shape[0]):
+                if (i % 30) == 0:
+                    prog = i/self.ctObject.quantitiesOfInterest[0].array.shape[0]*100 
+                    print("Calculating EUBED for " + r +  "... (" + str(round(prog,1))+"%)")
+                for j in range(self.ctObject.quantitiesOfInterest[0].array.shape[1]): 
+                    for k in range(self.ctObject.quantitiesOfInterest[0].array.shape[2]):
+                        if self.ctObject.structures3D[r][i,j,k] == True:
+                            darr[i,j,k] = self.ctObject.quantitiesOfInterest[0].array[i,j,k] 
+
+            # Calculate summation  
+            sum = 0
+            if r == 'Liver' or r == 'Lung_L' or r == 'Lung_R':
+                Alpha_ROI = Alpha_Normal
+            elif r == 'Right Tumor(s)' or r == 'Left Tumor(s)' or r == 'All Tumors (Right Lobe)' or r == 'All Tumors (Left Lobe)'
+                Alpha_ROI = Alpha_TLiver
+            else:
+                print('ROI type:' + r + ' not recognized')
+                Alpha_ROI = Alpha_Normal    
+            for i in range(darr.shape[0]):
+                for j in range(darr.shape[1]): 
+                    for k in range(darr.shape[2]):
+                        if darr[i,j,k] > 0:
+                            sum = sum + math.exp(-Alpha_ROI * darr[i,j,k])   
+     
+            # Calculate EUBED
+            N = (self.ctObject.structures3D[r]).sum()
+
+            EUBED = -1/Alpha_eq * np.log(sum / N)
+
+            print("EUBED for " + r + " =", EUBED, sum)   
 
 class DVH:
     def __init__(self, basepath, dosefile):
@@ -104,8 +127,6 @@ class DVH:
                     for k in range(self.ctObject.quantitiesOfInterest[0].array.shape[2]):
                         if self.ctObject.structures3D[r][i,j,k] == True:
                             darr[i,j,k] = self.ctObject.quantitiesOfInterest[0].array[i,j,k]   
-                        else:
-                            pass
             for h in range(bins):
                 hist[h] = (darr > (h * (maxdose/bins))).sum() / (darr > 0).sum() * 100
             self.curves.append(hist)
