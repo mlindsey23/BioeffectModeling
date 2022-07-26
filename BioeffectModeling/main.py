b0fe4225880947e57234e5c956cfeb8127069e22
mlindsey23 <107881040+mlindsey23@users.noreply.github.com>
2022-07-25 23:45:37 -0400

    EUBED write to file param

--- BioeffectModeling/main.py
+++ BioeffectModeling/main.py
@@ -4,7 +4,7 @@ from MIRDCalculation_BED.BioeffectModeling.EUBEDCalculator import *
 ### USER PARAMETERS ###
 
 # 1. Path to DICOM files (str)
-basepath = '/Users/mjlindsey/Documents/LiverPatients/Patient12/'
+basepath = '/Users/mjlindsey/Documents/LiverPatients/Patient1/POSTTX/'
 
 # 2. RTDOSE filename (str)
 dosefile = 'MIRDDose.dcm'
@@ -12,7 +12,10 @@ dosefile = 'MIRDDose.dcm'
 # 3. ROIs for EUBED Calculation (str, list)
 ROIList = ['Liver', 'Lung_L', 'Lung_R']
 
-# 4. Bins for DVH calculation (for faster processing) (posint)
+# 4. Create txt file of EUBED values (bin)
+CreateFile = True
+
+# 4. Bins for DVH calculation, to tune precision and processing time (posint)
 bins = 2000
 
 ### Biological Effective Dose ###
@@ -22,7 +25,7 @@ calc.WriteRTDoseBED()
 print("End BED Calculation.")
 
 ### EUBED ###
-calc.EUBEDCalculator(ROIList)
+calc.EUBEDCalculator(ROIList, CreateFile)
 
 ### DVH Curves ###
 dvhcalc = DVH(basepath, dosefile)
