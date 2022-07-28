4f0d19e2707958e0611faebd2b716b1ec9a404a8
mlindsey23 <107881040+mlindsey23@users.noreply.github.com>
2022-07-28 12:10:49 -0400

    Add more tumor struct cases

--- BioeffectModeling/EUBEDCalculator.py
+++ BioeffectModeling/EUBEDCalculator.py
@@ -51,7 +51,7 @@ class BioeffectCalculator(dcmpat.PatientCT):
                 for k in range(self.ctObject.quantitiesOfInterest[0].array.shape[2]):
                     if self.ctObject.structures3D['Liver'][i,j,k] == True :
                         try:
-                            if self.ctObject.structures3D['Right Tumor(s)'][i,j,k] == True or self.ctObject.structures3D['Left Tumor(s)'][i,j,k] == True or self.ctObject.structures3D['All Tumors (Left Lobe)'][i,j,k] == True or self.ctObject.structures3D['All Tumors (Right Lobe)'][i,j,k] == True:
+                            if self.ctObject.structures3D['Right Tumor(s)'][i,j,k] == True or self.ctObject.structures3D['Left Tumor(s)'][i,j,k] == True or self.ctObject.structures3D['All Tumors (Left Lobe)'][i,j,k] == True or self.ctObject.structures3D['All Tumors (Right Lobe)'][i,j,k] == True or self.ctObject.structures3D['Tumor 1'][i,j,k] == True or self.ctObject.structures3D['Tumor 2'][i,j,k] == True or self.ctObject.structures3D['Tumor 3'][i,j,k] == True:
                                 Trep = Trep_Tumor
                                 AlphaBeta = AlphaBeta_TLiver
                             else:
@@ -88,7 +88,7 @@ class BioeffectCalculator(dcmpat.PatientCT):
             sum = 0
             if r == 'Liver' or r == 'Lung_L' or r == 'Lung_R':
                 Alpha_ROI = Alpha_Normal
-            elif r == 'Right Tumor(s)' or r == 'Left Tumor(s)' or r == 'All Tumors (Right Lobe)' or r == 'All Tumors (Left Lobe)':
+            elif r == 'Right Tumor(s)' or r == 'Left Tumor(s)' or r == 'All Tumors (Right Lobe)' or r == 'All Tumors (Left Lobe)' or r == 'Tumor 1' or r == 'Tumor 2' or r == 'Tumor 3':
                 Alpha_ROI = Alpha_TLiver
             else:
                 print('ROI type:' + r + ' not recognized')
@@ -165,65 +165,3 @@ class BioeffectCalculator(dcmpat.PatientCT):
             seriesdescription = self.dosefilename + "_" + unitabbr
         name = self.dosefilename + '_' + unitabbr + '.dcm'
         self.ctObject.WriteRTDose(self.ctObject.quantitiesOfInterest[0].array, self.basepath + name, self.unit, seriesdescription)
-            
-class DVH:
-    def __init__(self, basepath, dosefile):
-        ctpath = basepath + '/CT/'
-        dosepath = basepath + dosefile
-        dosefile_full = os.path.basename(dosefile)
-        dosefile_split = dosefile_full.split('.')[0]
-        self.basepath = basepath
-        self.dosefilename = dosefile_split
-        self.patientObject = dcmpat.DicomPatient(basepath)
-        self.patientObject.dcmFileChosen = pydicom.dcmread(dosepath)
-        self.ctObject = dcmpat.PatientCT(ctpath)
-        self.ctObject.LoadRTDose(dosepath)
-        try:
-            structfile = os.listdir(basepath + '/RTSTRUCT/')
-            structpath = basepath + '/RTSTRUCT/' + structfile[0]
-            self.ctObject.LoadStructures(structpath)
-        except Exception as e: 
-            structfile = os.listdir(basepath + '/RTSTRUCT_LUNGSANDLIVER/')
-            structpath = basepath + '/RTSTRUCT_LUNGSANDLIVER/' + structfile[0]
-            self.ctObject.LoadStructures(structpath)
-            print('ERROR: Could not load complete RTSTRUCT. CODE:', e)
-            print('RTSTRUCT_LUNGSANDLIVER loaded instead.')
-        print("ROI's identified:", list(self.ctObject.structures3D.keys()))
-        self.curves = []
-        
-    def DVHCalculator(self, ROIList, bins = 1000):
-        for r in ROIList:
-            darr = np.zeros(self.ctObject.quantitiesOfInterest[0].array.shape) 
-            hist = [0] * bins
-            maxdose = round(self.ctObject.quantitiesOfInterest[0].array.max())
-            mindose = round(self.ctObject.quantitiesOfInterest[0].array.min())
-            for i in range(self.ctObject.quantitiesOfInterest[0].array.shape[0]):
-                if (i % 30) == 0:
-                    prog = i/self.ctObject.quantitiesOfInterest[0].array.shape[0]*100 
-                    print("Getting DVH for " + r +  "... (" + str(round(prog,1))+"%)")
-                for j in range(self.ctObject.quantitiesOfInterest[0].array.shape[1]): 
-                    for k in range(self.ctObject.quantitiesOfInterest[0].array.shape[2]):
-                        if self.ctObject.structures3D[r][i,j,k] == True:
-                            darr[i,j,k] = self.ctObject.quantitiesOfInterest[0].array[i,j,k]   
-            for h in range(bins):
-                hist[h] = (darr > (h * (maxdose/bins))).sum() / (darr > 0).sum() * 100
-            self.curves.append(hist)
-            print(r + ' DVH Calculated.')
-    
-    def PlotDVHCurves(self, ROIList, bins = 1000):
-        if self.ctObject.quantitiesOfInterest[0].array.max() < 0.1:
-            print('ERROR: Dose grid values too low to create DVH.')
-        try:
-            unit = str(self.patientObject.dcmFileChosen.DoseUnits)
-        except:
-            unit = 'arb. units'
-        maxdose = round(self.ctObject.quantitiesOfInterest[0].array.max())
-        xpts = np.linspace(0, maxdose, bins)
-        for r in ROIList:
-            plt.plot(xpts, self.curves[ROIList.index(r)])
-        plt.xlabel('Dose [' + unit + ']')
-        plt.ylabel("Volume [%]")
-        plt.legend(ROIList, loc="upper right")
-        plt.savefig(self.basepath + 'DVH_' + self.dosefilename + '.png')
-        plt.savefig(self.basepath + 'DVH_' + self.dosefilename + '.jpg')
-        plt.show()
