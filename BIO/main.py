135622ae8388b822048adc51f1b1001fabd93825
mlindsey23 <107881040+mlindsey23@users.noreply.github.com>
2022-07-28 14:13:31 -0400

    Add maxVoxel param

--- BIO/main.py
+++ BIO/main.py
@@ -7,20 +7,23 @@ from BioeffectModeling.BIO.EUBEDCalculator import *
 basepath = '/Users/mjlindsey/Documents/LiverPatients/Patient1/POSTTX/'
 
 # 2. RTDOSE filename (str)
-dosefile = 'MIRDDose.dcm'
+dosefile = 'TOPASDose.dcm'
 
-# 3. Units for BED RTDOSE file (str) (Choose from "Gy/Gbq", "Gy/mCi", or "Gy")
-unit = "Gy/GBq"
+# 3. Units for BED RTDOSE file (str) (Choose from "Gy/GBq", "Gy/mCi", or "Gy")
+unit = "Gy"
 
 # 4. ROIs for EUBED Calculation (str, list)
 ROIList = ['Liver', 'Lung_L', 'Lung_R']
 
+# 5. Print EUBED and EUD into txt file (bin)
+createFile = True
 
-
+# 6. Scale input dose file to a max value (float) (Note: Useful for low-dose TOPAS files) (optional)
+maxVoxel = 50
 
 ### Main Script ###
-calc = EUBEDCalculator(basepath, dosefile, unit)
+calc = EUBEDCalculator(basepath, dosefile, unit, maxVoxel)
 calc.BEDCalculator()    
 calc.WriteRTDoseBED()
-calc.EUBED(ROIList, CreateFile)
-calc.EUD(ROIList, CreateFile)
+calc.EUBED(ROIList, createFile)
+calc.EUD(ROIList, createFile)
